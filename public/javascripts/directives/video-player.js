0a3ff5b1df35fa473703f8380bf7b9712d9d7034
Jim Fowler <fowler@math.osu.edu>
2014-01-11 12:23:10 -0500

    Video player now syncs to the database

--- public/javascripts/directives/video-player.js
+++ public/javascripts/directives/video-player.js
@@ -9,20 +9,20 @@ define(['angular', 'jquery', 'underscore', 'youtube'], function(angular, $, _, Y
     'use strict';
 
     angular.module('ximeraApp.videoPlayer', [])
-	.directive('ximeraYoutube', ['$timeout', function($timeout) {
+	.directive('ximeraYoutube', ['$timeout', 'stateService', function($timeout, stateService) {
 	    return {
 		restrict: 'A',
-
-		scope: {
-		    seconds:'=seconds' // the current video position, in seconds
-		},
 
 		// there's a good reason to surround the YouTube by a div?
 		template: ('<div class="youtube" style="height:390px;"><div class="replaced-by-youtube-player"></div></div>'),
 		transclude: true,
 
-		link: function(scope, element, attrs, controller, transclude) {
+		link: function($scope, element, attrs, controller, transclude) {
 		    var code;
+
+                    stateService.bindState($scope, $(element).attr('data-uuid'), function () {
+			$scope.db.seconds = 0;
+                    });
 
                     // Extract YouTube code from original
                     transclude(function (clone) {
@@ -32,18 +32,18 @@ define(['angular', 'jquery', 'underscore', 'youtube'], function(angular, $, _, Y
 		    // When the player is ready, cue the video at the requested position
 		    var onPlayerReady = function(event) {
 			var player = event.target;
-			player.cueVideoById(code, scope.seconds);
+			player.cueVideoById(code, $scope.db.seconds);
 			
-			scope.$watch( "seconds", function() {
+			$scope.$watch( "db.seconds", function() {
 			    /* This is probably a terrible solution.  The
-			       problem is that below I update scope.seconds,
+			       problem is that below I update $scope.seconds,
 			       but I don't actually intend to seek.
 			       
 			       On the other hand, I can only seekTo keyframes
 			       ANYWAY, so maybe it is perfectly fine to avoid
 			       seeking unless the request moves us far enough. */
-			    if (player.getCurrentTime() - scope.seconds > 0.5)
-				player.seekTo( scope.seconds, true );
+			    if (player.getCurrentTime() - $scope.db.seconds > 0.5)
+				player.seekTo( $scope.db.seconds, true );
 			});
 		    };
 		    
@@ -52,7 +52,7 @@ define(['angular', 'jquery', 'underscore', 'youtube'], function(angular, $, _, Y
 			var player = event.target;
 			
 			var everySecond = function() {
-			    scope.seconds = player.getCurrentTime();
+			    $scope.db.seconds = player.getCurrentTime();
 			    if (player.getPlayerState() == 1) $timeout( everySecond, 1000 );
 			};
 			
