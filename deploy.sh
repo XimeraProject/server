4f20855f384e2ff5599bae2b03b465a8593e0a83
Jim Fowler <fowler@math.osu.edu>
2014-02-28 11:26:50 -0500

    Deployment script from any machine

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-if [ $(hostname) = ext ]; then
-    echo copy files...
-    rsync -avz -f"- .git/" . ximera:/var/www/apps/ximera
+if [ $(hostname) = ximera-1.asc.ohio-state.edu ]; then
+    echo On the deployment machine.
+    #echo stop old copies of app.js...
+    #./node_modules/forever/bin/forever stop app.js
+    #echo start a new copy of app.js...
+    #source environment.sh
+    #export DEPLOYMENT=production
+    #./node_modules/forever/bin/forever start -a -l forever.log -o out.log -e err.log app.js &
+else
+    echo not on the deployment machine...
+    echo copying environment to deployment machine...
+    rsync -avz -f"- .git/" environment.sh ximera:/var/www/apps/ximera
     echo ssh to the deploy machine...
     ssh ximera "cd /var/www/apps/ximera ; source deploy.sh"
 else
-    echo made it to the deploy machine!
-    echo stop old copies of app.js...
-    ./node_modules/forever/bin/forever stop app.js
-    echo start a new copy of app.js...
-    source environment.sh
-    export DEPLOYMENT=production
-    ./node_modules/forever/bin/forever start -a -l forever.log -o out.log -e err.log app.js &
 fi
