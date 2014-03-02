e496f10fbf28e5a1a15b13815d39093ea09b195d
Jim Fowler <fowler@math.osu.edu>
2014-03-02 18:31:12 -0500

    Production tasks added to task runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,47 @@ module.exports = function(grunt) {
 	    }
 	},
 
+	requirejs: {
+	    compile: {
+		options: {
+                    name: "../../components/almond/almond",
+		    include: "app",
+		    baseUrl: "public/javascripts",
+		    mainConfigFile: "public/javascripts/app.js",
+		    out: "public/javascripts/app.min.js",
+		    // necessary to keep angular working
+		    optimize: "uglify2",
+		    uglify2: {
+			// do not mangle functions parameters names---angular needs them!
+			mangle: false
+		    },
+		}
+	    }
+	},
+
+	less: {
+	    compile: {
+		options: {
+		    paths: ["components/bootstrap/less"],
+		    cleancss: true,
+		},
+		files: {
+		    "public/stylesheets/base.css": "public/stylesheets/base.less"
+		}
+	    }
+	},
+
+	cssmin: {
+	    combine: {
+		options: {
+		    root: '.'
+		},
+		files: {
+		    'public/static/base.min.css': ["public/stylesheets/base.css"]
+		}
+	    }
+	},
+
 	/****************************************************************/
 	watch: {
 	    jison: {
@@ -26,4 +67,6 @@ module.exports = function(grunt) {
 
     require('load-grunt-tasks')(grunt);    
     grunt.registerTask('default', ['jison']);
+
+    grunt.registerTask('production', ['jison', 'requirejs', 'less', 'cssmin']);
 };
