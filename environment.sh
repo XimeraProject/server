978c454012aae9564a91127eb7db5f6618e32ecd
Jim Fowler <fowler@math.osu.edu>
2014-03-05 12:51:09 -0500

    Removing environment.sh from the repo

--- environment.sh
+++
@@ -1,3 +0,0 @@
-export XIMERA_MONGO_URL=127.0.0.1
-export XIMERA_MONGO_DATABASE=test
-export XIMERA_FILTER_PATH=~/Dropbox/code/ximera-pandoc-filter/dist/build/ximera-pandoc-filter/ximera-pandoc-filter
