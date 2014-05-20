f4cbff7e281c2df7e86a58c08d3537f77a885d94
Jim Fowler <fowler@math.osu.edu>
2014-05-20 13:27:18 -0400

    Using verifir

--- passport-lti.js
+++ passport-lti.js
@@ -4,7 +4,8 @@ var util = require('util')
   , lti = require("ims-lti");
 
 function LtiStrategy(options, verify) {
-    this.name = 'lti'
+    this.name = 'lti'  
+    this._verify = verify;
     this.provider = new lti.Provider(options.consumerKey, options.consumerSecret);
     passport.Strategy.call(this, options, verify);
 }
@@ -15,13 +16,19 @@ LtiStrategy.prototype.authenticate = function(req) {
     // I'm behind nginx so it looks like I'm serving http, but as far as the rest of the world is concerned, it's https
     var myRequest = _.extend({}, req, {protocol: 'https'});
     var self = this;
+    
+    function verified(err, user, info) {
+	if (err) { return self.error(err); }
+	if (!user) { return self.fail(info); }
+	self.success(user, info);
+    }
 
-    this.provider.valid_request(myRequest, function(err, isValid) {
+    slf.provider.valid_request(myRequest, function(err, isValid) {
 	if (!isValid) {
 	    return self.error(err);
+	} else {
+	    self._verify( req, 'identifier', 'profile', verified );
 	}
-
-	self.success(req.body);
     });
 }
 
