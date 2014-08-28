a515024fa8552cdf5bc92cd70f8a7d9cd78d03de
Jim Fowler <fowler@math.osu.edu>
2014-08-27 21:29:31 -0400

    Null is not the same as undefined for mongo

--- login.js
+++ login.js
@@ -35,6 +35,7 @@ module.exports.ltiStrategy = function (rootUrl) {
         consumerSecret: process.env.LTI_SECRET,
     }, function (req, identifier, profile, done) {
 	var displayName = 'Remote User';
+
 	if ('lis_person_name_full' in profile)
 	    displayName = profile.lis_person_name_full;
 	var email = '';
@@ -45,6 +46,8 @@ module.exports.ltiStrategy = function (rootUrl) {
         addUserAccount(req, 'ltiId', identifier, displayName, email, profile.custom_ximera, done);
     });
 }
+
+// POST&https%3A%2F%2Fximera.osu.edu%2Flti%2F&basiclti_submit%3DLaunch%2520Endpoint%2520with%2520BasicLTI%2520Data%26context_id%3D11395970%26context_label%3Dmath_2162.02_au2014_20577%26context_title%3DAU14%2520MATH%25202162.02%2520-%2520Acc%2520Eng%2520Calc%25202%2520%252820577%2529%26context_type%3D%26custom_ximera%3Dkisonecat%252Fmultivariable-calculus%26ext_d2l_link_id%3D216%26ext_d2l_role%3D.Student%26ext_d2l_token_digest%3DHySPKFZ9SEdVLiThi0Tp2Um3EEI%253D%26ext_d2l_token_id%3D163319336%26ext_d2l_username%3Dfowler.291%26ext_tc_profile_url%3Dhttps%253A%252F%252Fcarmen.osu.edu%252Fd2l%252Fapi%252Fext%252F1.0%252Flti%252Ftcservices%26launch_presentation_locale%3DEN-US__%26lis_outcome_service_url%3Dhttps%253A%252F%252Fcarmen.osu.edu%252Fd2l%252Fle%252Flti%252FOutcome%26lis_person_contact_email_primary%3Dfowler.291%2540osu.edu%26lis_person_name_family%3DFowler%26lis_person_name_full%3DJames%2520Fowler%26lis_person_name_given%3DJames%26lis_result_sourcedid%3D4938ea27-4cd5-4fec-a6f6-3d6e141c1948%26lti_message_type%3Dbasic-lti-launch-request%26lti_version%3DLTI-1p0%26oauth_callback%3Dabout%253Ablank%26oauth_consumer_key%3D8SoyJOxqroPZ1t3CyG%26oauth_nonce%3D283323381%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1409110801%26oauth_version%3D1.0%26resource_link_description%3D%26resource_link_id%3D%26resource_link_title%3DXimera%26roles%3DInstructor%26tool_consumer_info_product_family_code%3Ddesire2learn%26tool_consumer_info_version%3D10.3.0%2520SP4%26tool_consumer_instance_contact_email%3D%26tool_consumer_instance_description%3D%26tool_consumer_instance_guid%3D%26tool_consumer_instance_name%3D%26user_id%3DOSU_Prod_499871
 
 // Add guest users account if not logged in.
 // TODO: Clean these out occasionally.
@@ -131,7 +134,7 @@ function addUserAccount(req, authField, authId, name, email, course, done) {
         else {
             mdb.User.find(searchFields, function (err, users) {
                 async.eachSeries(users, function (user, callback) {
-                    user[authField] = null;
+                    user[authField] = undefined;
                     user.save(callback);
                 }, function (err) {
                     if (err) {
