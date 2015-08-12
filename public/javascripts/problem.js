f2b95498b01b4019b9c9f17897cb54e2cf26628f
Jim Fowler <fowler@math.osu.edu>
2015-08-12 22:08:30 +0200

    Rerender when a question is uncovered

--- public/javascripts/problem.js
+++ public/javascripts/problem.js
@@ -13,6 +13,7 @@ define(['jquery', 'underscore', 'mathjax', 'database', 'tincan'], function($, _,
 	    problem.persistentData( function(event) {
 		if (problem.persistentData( 'available' ) && !(problem.persistentData( 'experienced' ))) {
 		    TinCan.experience(problem);
+		    MathJax.Hub.Queue(["Rerender", MathJax.Hub, problem]);
 		    problem.persistentData( 'experienced', true );
 		}
 
