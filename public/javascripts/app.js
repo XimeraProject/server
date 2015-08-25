1e9a7ab78b41e801ccf4bc07db52e93a70307646
Jim Fowler <fowler@math.osu.edu>
2015-08-25 07:36:25 -0400

    Include mathjax in the minified javascript

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,7 +18,8 @@ require.config({
     ],
     
     paths: {
-        mathjax: "//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML&amp;delayStartupUntil=configured",
+        //mathjax: "//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML&amp;delayStartupUntil=configured",
+	mathjax: "../../components/MathJax/MathJax",
 	less: "../../components/less/dist/less.min",
 	"async": "../../components/async/lib/async",
 	"sly": "../../components/sly/dist/sly.min",
