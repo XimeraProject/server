f67bf85d05b5a73bbcfd194a1e67783e40fbbadb
Jim Fowler <fowler@math.osu.edu>
2016-07-24 15:41:23 -0400

    Added clickable rows to user index

---
+++ public/javascripts/rowclick.js
@@ -0,0 +1,18 @@
+var $ = require('jquery');
+
+// transforms any bootstrap table row that has data-href attribute set into a clickable element
+exports.addClickableTableRows = function() {
+    $('.table tr[data-href]').each(function(){
+        $(this).css('cursor','pointer').hover(
+            function(){ 
+                $(this).addClass('active'); 
+            },  
+            function(){ 
+                $(this).removeClass('active'); 
+            }).click( function(){ 
+                document.location = $(this).attr('data-href'); 
+            }
+        );
+    });
+};
+
