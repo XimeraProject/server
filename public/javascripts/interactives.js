834b3a5463bd485e43052a4b7857f580b1c789a4
Jim Fowler <fowler@math.osu.edu>
2016-07-27 09:25:10 -0400

    Sometimes there are no interactives

--- public/javascripts/interactives.js
+++ public/javascripts/interactives.js
@@ -7,13 +7,15 @@ var libraries = {
 };
 
 exports.connectInteractives = function() {
-    window.interactives.forEach( function(interactive) {
-	var dependencies = interactive.dependencies;
-	var callback = interactive.callback;
-
-	var targetId = interactive.targetId;
-	var target = $("#" + targetId);
-	
-	callback.apply( target, dependencies.map( function(name) { return libraries[name]; } ) );
-    });
+    if (window.interactives) {
+	window.interactives.forEach( function(interactive) {
+	    var dependencies = interactive.dependencies;
+	    var callback = interactive.callback;
+	    
+	    var targetId = interactive.targetId;
+	    var target = $("#" + targetId);
+	    
+	    callback.apply( target, dependencies.map( function(name) { return libraries[name]; } ) );
+	});
+    }
 };
