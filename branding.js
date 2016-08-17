2011fb0d42b68771e02ad308789af9500a5892b7
Jim Fowler <fowler@math.osu.edu>
2016-08-17 11:06:08 -0400

    Branding for multiple institutions

--- branding.js
+++ branding.js
@@ -1,25 +1,43 @@
 var ip = require('ip');
 var _ = require('underscore');
 
-var subnets = [ip.cidrSubnet('164.107.0.0/16'),
-	       ip.cidrSubnet('140.254.0.0/16'),
-	       ip.cidrSubnet('128.146.0.0/16'),
-	       ip.cidrSubnet('192.68.143.0/24'),
-	       ip.cidrSubnet('192.12.205.0/24')];
+var brandings = {
+    "localhost": [ip.cidrSubnet('127.0.0.0/8')],
+    "The Ohio State University": [ip.cidrSubnet('164.107.0.0/16'),
+				  ip.cidrSubnet('140.254.0.0/16'),
+				  ip.cidrSubnet('128.146.0.0/16'),
+				  ip.cidrSubnet('192.68.143.0/24'),
+				  ip.cidrSubnet('192.12.205.0/24')],
+    "Colorado State University": [ip.cidrSubnet('129.82.0.0/16')]
+};
+
 
 exports.middleware = function(req, res, next) {
     var remoteAddress = req.headers['x-forwarded-for'] || 
 	    req.connection.remoteAddress || 
 	    req.socket.remoteAddress ||
 	    req.connection.socket.remoteAddress;
-
     try {
-	if (_.some( subnets, function(subnet) { return subnet.contains(remoteAddress); } ))
+	res.locals.places = [];
+
+	Object.keys(brandings).forEach( function(place) {
+	    if (_.some( brandings[place], function(subnet) { return subnet.contains(remoteAddress); } )) {
+		res.locals.places.push( place );
+	    }
+	});
+	
+	if (res.locals.places.indexOf( "The Ohio State University" ) >= 0)
 	    res.locals.atOhioState = true;
 	else
-	    res.locals.atOhioState = false;	
+	    res.locals.atOhioState = false;
+
+	if (res.locals.places.indexOf( "Colorado State University" ) >= 0)
+	    res.locals.atColoradoState = true;
+	else
+	    res.locals.atColoradoState = false;
     }
-    catch (e) {    
+    catch (e) {
+	res.locals.places = [];
 	res.locals.atOhioState = false;
     }
     
