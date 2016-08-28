1014d4859bc7f7f868fd90198d7b0a48250f8a6a
Jim Fowler <fowler@math.osu.edu>
2016-08-28 18:48:25 -0400

    Xourse pages now match nav bar

--- public/javascripts/xourse.js
+++ public/javascripts/xourse.js
@@ -4,42 +4,10 @@ var Isotope = require('isotope-layout');
 
 var activityCard = require('./activity-card');
 
-var layoutXourse = function( xourseData ) {
-    console.log( xourseData );
-    
-    var xourse = $('.xourse-contents');
+var layoutXourse = function( ) {
+    var xourse = $(this);
 
-    var cards = $('h1, h2, h3, a.activity', xourseData);
-
-    cards.each( function() {
-	var cardData = $(this);
-	console.log( cardData );
-	
-	var card = $('<div class="activity-card"></div>');
-
-	var type = cardData.prop('tagName');
-	
-	if (type === 'H1') {
-	    card = $('<div class="activity-card large-card"></div>');
-	}
-
-	if (type === 'H2') {
-	    card = $('<div class="activity-card medium-card"></div>');
-	}
-
-	if (type === 'A') {
-	    card = $('<a class="activity-card"></a>');
-	    card.attr('data-path', cardData.attr('href') );
-	    card.attr('href', cardData.attr('href') );
-	    
-	    cardData.remove();
-	} else
-	    card.append( cardData );
-
-	xourse.append(card);
-    });
-
-    $('a.activity-card', xourse).activityCard();
+    $('.activity-card', xourse).activityCard();
     
     xourse.show();
     
@@ -61,9 +29,6 @@ var layoutXourse = function( xourseData ) {
 
 // On document ready...
 $(function() {
-    $('.xourse-data').each( function() {
-	var xourseData = $(this);
-	layoutXourse( xourseData );
-    });
+    $('.xourse').each( layoutXourse );
 });
 
