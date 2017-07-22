b6ceed1e51d96e5a208bd3da94a33ea15a3cf24d
Jim Fowler <fowler@math.osu.edu>
2017-07-22 19:32:05 -0400

    no more templating

--- routes/template.js
+++
@@ -1,7 +0,0 @@
-exports.renderTemplate = function(req, res){
-    res.render("templates/" + req.params.templateFile);
-};
-
-exports.renderForumTemplate = function(req, res){
-    res.render("templates/forum/" + req.params.templateFile);
-};
