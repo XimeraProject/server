7611f7bb760098049743bc39ed4179a5af79ff5e
Jim Fowler <fowler@math.osu.edu>
2017-07-23 16:27:48 -0400

    New deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $(hostname) = ximera-1.asc.ohio-state.edu ]; then
     echo On the deployment machine.
-    echo Pulling latest version from github...
-    mv -f environment.sh environment.backup
+    echo Pulling latest version from github, protecting our dotenv...
+    mv -f .env .env.backup
     git pull
-    mv -f environment.backup environment.sh
+    mv -f .env.backup .env
     echo Updating npm...
     npm install
     echo Running gulp...
@@ -13,13 +13,13 @@ if [ $(hostname) = ximera-1.asc.ohio-state.edu ]; then
     echo Stopping old copies of app.js...
     pm2 stop ximera
     echo Starting a new copy of app.js...
-    source environment.sh
     export DEPLOYMENT=production
     export NODE_ENV=production
+    export XIMERA_MONGO_DATABASE=production    
     pm2 start ecosystem.config.js --env production    
 else
     echo not on the deployment machine...
     echo copying environment and keys to deployment machine...
-    rsync -avz -f"- .git/" private_key.pem environment.sh ximera:/var/www/apps/ximera
+    rsync -avz -f"- .git/" private_key.pem .env ximera:/var/www/apps/ximera
     ssh ximera "cd /var/www/apps/ximera ; source deploy.sh"
 fi
