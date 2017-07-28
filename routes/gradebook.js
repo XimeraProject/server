ad8f48c94c223e5a463996fd3ea656efa23fc9af
Jim Fowler <fowler@math.osu.edu>
2017-07-28 14:15:39 -0400

    do the gradebook passback asynchronously

--- routes/gradebook.js
+++ routes/gradebook.js
@@ -4,6 +4,7 @@ var request = require('request');
 var pug = require('pug');
 var path = require('path');
 var config = require('../config');
+var async = require('async');
 const uuidv1 = require('uuid/v1');
 
 var passback = pug.compileFile(path.join(__dirname,'../views/lti/passback.pug'));
@@ -18,48 +19,55 @@ exports.record = function(req, res, next) {
 	    if (err) {
 		next(err);
 	    } else {
-		bridges.forEach( function(bridge) {
-		    var pointsPossible = parseInt(bridge.data.custom_canvas_assignment_points_possible);
-		    var resultScore = parseFloat(req.body.pointsEarned) / parseFloat(req.body.pointsPossible);
-		    var resultTotalScore = resultScore * pointsPossible;
-		    
-		    var pox = passback({
-			messageIdentifier: uuidv1(),
-			resultDataUrl: config.root + '/users/' + req.user._id + '/' + repositoryName + '/' + req.params.path,
-			resultScore: resultScore,
-			resultTotalScore: resultTotalScore,
-			sourcedId: bridge.data.lis_result_sourcedid
-		    });
-
-		    var url = bridge.data.lis_outcome_service_url;
-		    
-		    var oauth = {
-			callback: "about:blank",
-			body_hash: true,			
-			consumer_key: bridge.data.oauth_consumer_key,
-			consumer_secret: config.lti.secret,
-			signature_method: bridge.data.oauth_signature_method
-		    };
-
-		    if (oauth.consumer_key != config.lti.key) {
-			console.log("WRONG KEY");
-		    }
-		    
-		    request.post({
-			url: url,
-			body: pox,
-			oauth: oauth,
-			headers: {
-			    'Content-Type': 'application/xml',
-			}
-		    }, function(err, response, body) {
-			if (err) {
-			    res.status(500).json(err);			    
-			} else {
-			    res.json({ok: true});			    
-			}
-		    });
-		});
+		_.each( bridges,
+			function(bridge, callback) {
+			    var pointsPossible = parseInt(bridge.data.custom_canvas_assignment_points_possible);
+			    var resultScore = parseFloat(req.body.pointsEarned) / parseFloat(req.body.pointsPossible);
+			    var resultTotalScore = resultScore * pointsPossible;
+			    
+			    var pox = passback({
+				messageIdentifier: uuidv1(),
+				resultDataUrl: config.root + '/users/' + req.user._id + '/' + repositoryName + '/' + req.params.path,
+				resultScore: resultScore,
+				resultTotalScore: resultTotalScore,
+				sourcedId: bridge.data.lis_result_sourcedid
+			    });
+			    
+			    var url = bridge.data.lis_outcome_service_url;
+			    
+			    var oauth = {
+				callback: "about:blank",
+				body_hash: true,			
+				consumer_key: bridge.data.oauth_consumer_key,
+				consumer_secret: config.lti.secret,
+				signature_method: bridge.data.oauth_signature_method
+			    };
+			    
+			    if (oauth.consumer_key != config.lti.key) {
+				console.log("WRONG KEY");
+			    }
+			    
+			    request.post({
+				url: url,
+				body: pox,
+				oauth: oauth,
+				headers: {
+				    'Content-Type': 'application/xml',
+				}
+			    }, function(err, response, body) {
+				if (err) {
+				    callback(err);
+				} else {
+				    callback(null);
+				}
+			    });
+			},
+			function(err) {
+			    if (err) 
+				res.status(500).json(err);			    
+			    else
+				res.json({ok: true});			    
+			});
 	    }
 	});
     }
