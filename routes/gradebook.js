e04ff09d744a983ca5c7a08f6895d52c15277d46
Jim Fowler <fowler@math.osu.edu>
2017-07-28 14:16:33 -0400

    wrong async library

--- routes/gradebook.js
+++ routes/gradebook.js
@@ -19,55 +19,55 @@ exports.record = function(req, res, next) {
 	    if (err) {
 		next(err);
 	    } else {
-		_.each( bridges,
-			function(bridge, callback) {
-			    var pointsPossible = parseInt(bridge.data.custom_canvas_assignment_points_possible);
-			    var resultScore = parseFloat(req.body.pointsEarned) / parseFloat(req.body.pointsPossible);
-			    var resultTotalScore = resultScore * pointsPossible;
-			    
-			    var pox = passback({
-				messageIdentifier: uuidv1(),
-				resultDataUrl: config.root + '/users/' + req.user._id + '/' + repositoryName + '/' + req.params.path,
-				resultScore: resultScore,
-				resultTotalScore: resultTotalScore,
-				sourcedId: bridge.data.lis_result_sourcedid
-			    });
-			    
-			    var url = bridge.data.lis_outcome_service_url;
-			    
-			    var oauth = {
-				callback: "about:blank",
-				body_hash: true,			
-				consumer_key: bridge.data.oauth_consumer_key,
-				consumer_secret: config.lti.secret,
-				signature_method: bridge.data.oauth_signature_method
-			    };
-			    
-			    if (oauth.consumer_key != config.lti.key) {
-				console.log("WRONG KEY");
-			    }
-			    
-			    request.post({
-				url: url,
-				body: pox,
-				oauth: oauth,
-				headers: {
-				    'Content-Type': 'application/xml',
+		async.each( bridges,
+			    function(bridge, callback) {
+				var pointsPossible = parseInt(bridge.data.custom_canvas_assignment_points_possible);
+				var resultScore = parseFloat(req.body.pointsEarned) / parseFloat(req.body.pointsPossible);
+				var resultTotalScore = resultScore * pointsPossible;
+				
+				var pox = passback({
+				    messageIdentifier: uuidv1(),
+				    resultDataUrl: config.root + '/users/' + req.user._id + '/' + repositoryName + '/' + req.params.path,
+				    resultScore: resultScore,
+				    resultTotalScore: resultTotalScore,
+				    sourcedId: bridge.data.lis_result_sourcedid
+				});
+				
+				var url = bridge.data.lis_outcome_service_url;
+				
+				var oauth = {
+				    callback: "about:blank",
+				    body_hash: true,			
+				    consumer_key: bridge.data.oauth_consumer_key,
+				    consumer_secret: config.lti.secret,
+				    signature_method: bridge.data.oauth_signature_method
+				};
+				
+				if (oauth.consumer_key != config.lti.key) {
+				    console.log("WRONG KEY");
 				}
-			    }, function(err, response, body) {
-				if (err) {
-				    callback(err);
-				} else {
-				    callback(null);
-				}
+				
+				request.post({
+				    url: url,
+				    body: pox,
+				    oauth: oauth,
+				    headers: {
+					'Content-Type': 'application/xml',
+				    }
+				}, function(err, response, body) {
+				    if (err) {
+					callback(err);
+				    } else {
+					callback(null);
+				    }
+				});
+			    },
+			    function(err) {
+				if (err) 
+				    res.status(500).json(err);			    
+				else
+				    res.json({ok: true});			    
 			    });
-			},
-			function(err) {
-			    if (err) 
-				res.status(500).json(err);			    
-			    else
-				res.json({ok: true});			    
-			});
 	    }
 	});
     }
