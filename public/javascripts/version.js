c1411a81e3c5d39fe54cf5f86a6e09e1918ba356
Jim Fowler <fowler@math.osu.edu>
2018-04-18 10:45:31 -0400

    Slightly safer cache-busting

--- public/javascripts/version.js
+++ public/javascripts/version.js
@@ -7,22 +7,28 @@ console.log("This is XIMERA, Version " + version );
 $(function() {
     // Check which version the server is providing, avoiding the cache
     $.ajax( "/version?" + (new Date().getTime()) )
-	.done(function(data) {
+	.done(function(data) {    
 	    // If the server can offer a newer version, let's update
 	    if (data != version) {
-		console.log("Uppdating from version " + version + " to version " + data );
+		if (sessionStorage.getItem('refreshedToVersion') == data) {
+		    alert('Attempted to refresh; try a force refresh.');
+		} else {
+		    sessionStorage.setItem('refreshedToVersion',  data);
+		    
+		    console.log("Uppdating from version " + version + " to version " + data );
 
-		// This is needed in Chrome
-		$.ajax({
-		    url: window.location.href,
-		    headers: {
-			"Pragma": "no-cache",
-			"Expires": -1,
-			"Cache-Control": "no-cache"
-		    }
-		}).done(function () {
-		    window.location.reload(true);
-		});
+		    // This is MAYBE needed in Chrome
+		    $.ajax({
+			url: window.location.href,
+			headers: {
+			    "Pragma": "no-cache",
+			    "Expires": -1,
+			    "Cache-Control": "no-cache"
+			}
+		    }).done(function () {
+			window.location.reload(true);
+		    });
+		}
 	    }
 	});
 });
